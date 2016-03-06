6ed4bf52248ad6bb8b62e1eb959a1afcb17e090d
pedram <pedram@eyelevate.com>
2016-03-06 07:07:14 -0700

    'update'

--- public/assets/js/layouts/main.js
+++ public/assets/js/layouts/main.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
 mainf = {
 	pageLoad: function() {
 		// getContent();
+		if ( (location.hash == "#_=_" || location.href.slice(-1) == "#_=_") ) {
+		    removeHash();
+		}
 		$.ajaxSetup({
 			headers: { 'X-CSRF-Token' : $('meta[name=csrf-token]').attr('content') }
 		});
@@ -355,3 +358,19 @@ function renew_subcat(t_v)
 	$('#subcat-select').html(select_html);
 }
 
+function removeHash() {
+    var scrollV, scrollH, loc = window.location;
+    if ('replaceState' in history) {
+        history.replaceState('', document.title, loc.pathname + loc.search);
+    } else {
+        // Prevent scrolling by storing the page's current scroll offset
+        scrollV = document.body.scrollTop;
+        scrollH = document.body.scrollLeft;
+
+        loc.hash = '';
+
+        // Restore the scroll offset, should be flicker free
+        document.body.scrollTop = scrollV;
+        document.body.scrollLeft = scrollH;
+    }
+}
