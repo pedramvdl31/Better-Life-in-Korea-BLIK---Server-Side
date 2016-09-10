6c34e0d54d34068051dd300fd1cf8fdd9c5087b3
pedram31 <pedram@eyelevate.com>
2016-09-10 23:01:12 +0900

    'update'

--- public/assets/js/layouts/popup.js
+++ public/assets/js/layouts/popup.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
 	var this_id = document.getElementById("popup_id").value;
 	$('#atwl-btn').attr('data',this_id);
 	$('.fbc').html('');
-	HelperFuncions.findAndViewAd(this_id);
+	setTimeout(function(){ 
+		HelperFuncions.findAndViewAd(this_id);
+	 }, 1000);
+	
 });
