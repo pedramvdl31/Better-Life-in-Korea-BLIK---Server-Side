746222487c5c6c96ee5665af4329ce458b253911
pedram31 <pedram@eyelevate.com>
2016-09-25 12:13:23 +0900

    'update'

--- resources/views/partials/categories.blade.php
+++ resources/views/partials/categories.blade.php
@@ -1,6 +1,32 @@
+<style type="text/css">
+    #btcd{
+        float:left;
+        margin-top: 20px;         
+    }
+    /* Small Devices, Tablets */
+    @media only screen and (max-width : 768px) {
+        #btcd{
+            margin-top: 0;         
+        }
+    }
+
+    /* Extra Small Devices, Phones */ 
+    @media only screen and (max-width : 480px) {
+        #btcd{
+            margin-top: 0;         
+        }
+    }
+
+    /* Custom, iPhone Retina */ 
+    @media only screen and (max-width : 320px) {
+        #btcd{
+            margin-top: 0;         
+        }
+    }
+</style>
 <div id="app-view" class="container">
     <div class="col-sm-12 ads-warps all-tabs cat-tab">
-        <div style="float:left;margin-top: 20px;">
+        <div id="btcd" style="">
             <a id="btc" class="btn btn-primary btn-md"><i class="glyphicon glyphicon-chevron-left"></i></a>
         </div>
         <h2 class="pages-title oswald-regular" class="title text-center">Categories</h2>
