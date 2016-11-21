1551c15c9cb3db4e817b761e65cc364e8d2b1fb9
pedram31 <pedram@eyelevate.com>
2016-11-21 23:24:00 +0900

    'bug'

--- app/Ad.php
+++ app/Ad.php
@@ -241,8 +241,7 @@ class Ad extends Model
                 $new_des = '';
                 if (isset($dv['title'])) {
                     $t_temp = utf8_encode($dv['title']);
-                    $new_ten = strlen($t_temp)>20?mb_substr($t_temp,0,20, "utf-8")."...":$t_temp;
-                    $new_t = $t_temp;
+                    $new_t = strlen($t_temp)>20?mb_substr($t_temp,0,20, "utf-8")."...":$t_temp;
                 }
                 if (isset($dv['description'])) {
                     $des_temp = json_decode($dv['description']);
