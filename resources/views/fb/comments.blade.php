dab26ec2c58ffd60077578f4c0130db06c3f508f
pedram31 <pedram@eyelevate.com>
2017-01-08 11:44:58 +0900

    'test'

--- resources/views/fb/comments.blade.php
+++ resources/views/fb/comments.blade.php
@@ -7,6 +7,13 @@
 @section('content')
 	<script type="text/javascript">
 	</script>
+	@if(Auth::check())
+
+	@else
+	    <a class="btn btn-social btn-facebook" href="/auth/facebook">
+			<span class="fa fa-facebook"></span>  Sign in with Facebook
+		</a>
+	@endif
 	<div class="fb-comments modalfc"  data-href={!!Request::root()!!}/posts/{{$pid}}
 	data-width="100%" data-numposts="5">
 	</div>
