49f4ebe969b93c37ecb605434fbcbed5b481ca56
pedram31 <pedram@eyelevate.com>
2017-02-11 20:15:46 +0900

    'update'

--- app/Ad.php
+++ app/Ad.php
@@ -804,8 +804,8 @@ class Ad extends Model
             $data_array['coms'] .=    '<li id="snd-li" class="clearfix">
                                   <div class="post-comments sendcomment">
                                     <textarea class="" placeholder="Write a Review"></textarea>
-                                    <a href="#" class="btn btn-default btn-sm rvcom">Send</a>
-                                    <input name="comment-rev" type="number" class="rating crev">
+                                    <a href="#" class="btn btn-default btn-sm rvcom" revstar="999" >Send</a>
+                                    <input name="comment-rev" type="number" class="rating sbtnrev">
                                   </div>
                                 </li>
                               </ul>';
