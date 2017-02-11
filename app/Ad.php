3ed0015539c65eee4a0939f058ebc9b252365e7d
pedram31 <pedram@eyelevate.com>
2017-02-11 20:41:50 +0900

    'update'

--- app/Ad.php
+++ app/Ad.php
@@ -788,7 +788,7 @@ class Ad extends Model
                         $curu = User::where('api_token',$user_token)->first();
                         if (isset($curu)&&!empty($curu)) {
                             if ($curu->id == $vco['user_id']) {
-                                $data_array['coms'] .= '<i class="pull-right"><a class="delcom" href="#"><small>Delete</small></a></i><div class="comrw"><input name="input-name" type="number" class="rating comrate" min=1 max=10 step=0.5 data-size="xs" data-rtl="false" disabled="true" value="10"></div>';
+                                $data_array['coms'] .= '<i class="pull-right"><a class="delcom" href="#"><small>Delete</small></a><div class="comrw"><input name="input-name" type="number" class="rating comrate" min=1 max=10 step=0.5 data-size="xs" data-rtl="false" disabled="true" value="10"></div></i>';
                             }
                         }
                     }                            
