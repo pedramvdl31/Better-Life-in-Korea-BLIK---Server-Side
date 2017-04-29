a5ae66f0a5532939aa64a809ad6f9235a965afe6
pedram <pedram@eyelevate.com>
2017-04-29 16:26:50 +0900

    'update'

--- app/Job.php
+++ app/Job.php
@@ -58,20 +58,22 @@ class Job extends Model
 	      }
 	}
 
-	static public function haversineGreatCircleDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000)
-	{
-	  // convert from degrees to radians
-	  $latFrom = deg2rad($latitudeFrom);
-	  $lonFrom = deg2rad($longitudeFrom);
-	  $latTo = deg2rad($latitudeTo);
-	  $lonTo = deg2rad($longitudeTo);
-
-	  $latDelta = $latTo - $latFrom;
-	  $lonDelta = $lonTo - $lonFrom;
-
-	  $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
-	    cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
-	  return $angle * $earthRadius;
+	static public function distanceCalculation($point1_lat, $point1_long, $point2_lat, $point2_long, $unit = 'km', $decimals = 2) {
+		// Calculate the distance in degrees
+		$degrees = rad2deg(acos((sin(deg2rad($point1_lat))*sin(deg2rad($point2_lat))) + (cos(deg2rad($point1_lat))*cos(deg2rad($point2_lat))*cos(deg2rad($point1_long-$point2_long)))));
+	 
+		// Convert the distance in degrees to the chosen unit (kilometres, miles or nautical miles)
+		switch($unit) {
+			case 'km':
+				$distance = $degrees * 111.13384; // 1 degree = 111.13384 km, based on the average diameter of the Earth (12,735 km)
+				break;
+			case 'mi':
+				$distance = $degrees * 69.05482; // 1 degree = 69.05482 miles, based on the average diameter of the Earth (7,913.1 miles)
+				break;
+			case 'nmi':
+				$distance =  $degrees * 59.97662; // 1 degree = 59.97662 nautic miles, based on the average diameter of the Earth (6,876.3 nautical miles)
+		}
+		return round($distance, $decimals);
 	}
 
 	static public function ReturnBp() {
