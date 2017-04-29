6d6439aa789d69935900ab1bb0cbb3a61951b391
pedram <pedram@eyelevate.com>
2017-04-29 16:22:16 +0900

    'update'

--- app/Job.php
+++ app/Job.php
@@ -58,6 +58,22 @@ class Job extends Model
 	      }
 	}
 
+	static public function haversineGreatCircleDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000)
+	{
+	  // convert from degrees to radians
+	  $latFrom = deg2rad($latitudeFrom);
+	  $lonFrom = deg2rad($longitudeFrom);
+	  $latTo = deg2rad($latitudeTo);
+	  $lonTo = deg2rad($longitudeTo);
+
+	  $latDelta = $latTo - $latFrom;
+	  $lonDelta = $lonTo - $lonFrom;
+
+	  $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+	    cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+	  return $angle * $earthRadius;
+	}
+
 	static public function ReturnBp() {
         $bp = 'https://www.betterlifeinkorea.com';
         // $bp = 'http://kora.app:8000';
