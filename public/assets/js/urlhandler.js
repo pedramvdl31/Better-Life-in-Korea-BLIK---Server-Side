7ff33f6f4d0f223bd67aaecbd19833919515cdf3
pedram <pedram@eyelevate.com>
2017-05-29 15:31:38 +0900

    'laptop'

--- public/assets/js/urlhandler.js
+++ public/assets/js/urlhandler.js
@@ -4,16 +4,22 @@ $(document).ready(function(){
 });
 uh = {
 	pageLoad: function() {
-			if (adid==0) {
-				window.location.href = 'blikapp://';
-			} else {
-				// window.location.href = 'blikapp://?adid='+adid;
-				window.open(
-				  'blikapp://?adid='+adid,
-				  '_blank' // <- This is what makes it open in a new window.
-				);
-			}
-			
+			// if (adid==0) {
+			// 	window.location.href = 'blikapp://';
+			// } else {
+			// 	// window.location.href = 'blikapp://?adid='+adid;
+			// 	window.open(
+			// 	  'blikapp://?adid='+adid,
+			// 	  '_blank' // <- This is what makes it open in a new window.
+			// 	);
+			// }
+			    function open_appstore() {
+			        window.location='http://itunes.com/';
+			    }
+
+			    function try_to_open_app() {
+			        setTimeout('open_appstore()', 300);
+			    }
 		},
 	events: function() {
 
