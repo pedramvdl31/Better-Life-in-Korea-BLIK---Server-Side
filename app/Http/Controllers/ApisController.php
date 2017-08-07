892c3e77c6d49aa2173472db5f6c1061ac2c7fdf
pedram <pedram@eyelevate.com>
2017-08-07 23:30:06 +0900

    'followers'

--- app/Http/Controllers/ApisController.php
+++ app/Http/Controllers/ApisController.php
@@ -795,7 +795,8 @@ class ApisController extends Controller
         $this_ad = Ad::where('id',$id)->first();
         $this_user = User::where('id',$this_ad->user_id)->first();
         if (isset($this_user)&&!empty($this_user)) {
-            $followers = count(Follow::where('followe_id',(int)$this_user->id)->get());
+            Job::dump($this_user->id);
+            $followers = count(Follow::where('followe_id',$this_user->id)->get());
             $user_img = $this_user->avatar;
             $base_url = '/assets/images/posts/'.$this_user->id.'/prm/image/';
             $user_avatar = (isset($user_img))?$base_url.$user_img:$base_url.'blank_male.png';
