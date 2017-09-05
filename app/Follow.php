83936b6eaeb1ddd800511006f6515c63f3cb535d
pedram <pedram@eyelevate.com>
2017-09-05 22:52:48 +0900

    'update'

--- app/Follow.php
+++ app/Follow.php
@@ -49,28 +49,23 @@ class Follow extends Model
     		$followee = Follow::where('follower_id',$this_user->id)->get();
     		if ($followee) {
     			foreach ($followee as $flek => $flev) {
-                    $data_array[$count]['date'] = $flev->created_at;
-                    $data_array[$count]['type'] = 'following';
-                    $data_array[$count]['m'] = date('M', strtotime($flev->created_at));
-                    $data_array[$count]['j'] = date('j', strtotime($flev->created_at));
+                    $data_array[strtotime($flev->created_at)['date'] = $flev->created_at;
+                    $data_array[strtotime($flev->created_at)]['type'] = 'following';
+                    $data_array[strtotime($flev->created_at)]['m'] = date('M', strtotime($flev->created_at));
+                    $data_array[strtotime($flev->created_at)]['j'] = date('j', strtotime($flev->created_at));
 					$user_followee = User::find($flev->followe_id);
 					if ($user_followee) {
-                        $data_array[$count]['email'] = $user_followee->email;
-                        $data_array[$count]['id'] = $user_followee->id;
+                        $data_array[strtotime($flev->created_at)]['email'] = $user_followee->email;
+                        $data_array[strtotime($flev->created_at)]['id'] = $user_followee->id;
 					}
                     $count++;
     			}
     		}
     	}
 
-        usort($data_array, 'compare');
-        
-    function compare($a, $b)
-    {
-        return strtotime($a['date']) - strtotime($b['date']);
-    }
         return $data_array;
     }
+
 
 
 }
