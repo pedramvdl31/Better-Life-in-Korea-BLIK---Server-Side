5030f4457177c47aabbea3846ad73d7202657d4f
pedram <pedram@eyelevate.com>
2017-11-19 12:35:40 +0900

    'update'

--- app/Follow.php
+++ app/Follow.php
@@ -30,18 +30,25 @@ class Follow extends Model
         if ($all_f) {
             foreach ($all_f as $k => $flv) {
                 $data_array[$k]['date'] = $flv->created_at;
+                $data_array[$k]['email'] = '';
+                $data_array[$k]['id'] = '';
                 if ($flv->follower_id == $this_user->id) {
                     $data_array[$k]['type'] = 'follower';
+                    $user_followe = User::find($flv->followe_id);
+                    if ($user_followe) {
+                        $data_array[$k]['email'] = $user_followe->email;
+                        $data_array[$k]['id'] = $user_followe->id;
+                    }
                 } else {
                     $data_array[$k]['type'] = 'following';
+                    $user_follower = User::find($flv->follower_id);
+                    if ($user_follower) {
+                        $data_array[$k]['email'] = $user_follower->email;
+                        $data_array[$k]['id'] = $user_follower->id;
+                    }
                 }
                 $data_array[$k]['m'] = date('M', strtotime($flv->created_at));
                 $data_array[$k]['j'] = date('j', strtotime($flv->created_at));
-                $user_follower = User::find($flv->follower_id);
-                if ($user_follower) {
-                    $data_array[$k]['email'] = $user_follower->email;
-                    $data_array[$k]['id'] = $user_follower->id;
-                }
                 $count++;
             }
         }
