de90178f4e9c33d30afb08bea3001f5c7a251deb
pedram <pedram@eyelevate.com>
2017-11-19 12:28:45 +0900

    'update'

--- app/Follow.php
+++ app/Follow.php
@@ -24,33 +24,27 @@ use App\Follow;
 class Follow extends Model
 {
     static public function PrepareFollowPageData($this_user) {
-    	$data_array=null;
-    	if (isset($this_user)) {
-    		$data_array=array();
-            $count = 0 ;
-    		$all_f = Follow::where('status',1)->orderBy('created_at','desc')->get();
-
-            if ($all_f) {
-                foreach ($all_f as $k => $flv) {
-                    $data_array[$k]['date'] = $flv->created_at;
-                    if ($flv->follower_id == $this_user->id) {
-                        $data_array[$k]['type'] = 'follower';
-                    } else {
-                        $data_array[$k]['type'] = 'following';
-                    }
-                    $data_array[$k]['m'] = date('M', strtotime($flv->created_at));
-                    $data_array[$k]['j'] = date('j', strtotime($flv->created_at));
-                    $user_follower = User::find($flv->follower_id);
-                    if ($user_follower) {
-                        $data_array[$k]['email'] = $user_follower->email;
-                        $data_array[$k]['id'] = $user_follower->id;
-                    }
-                    $count++;
+		$data_array=array();
+        $count = 0 ;
+		$all_f = Follow::where('status',1)->where('followe_id',$this_user->id)->orWhere('follower_id',$this_user->id)->orderBy('created_at','desc')->get();
+        if ($all_f) {
+            foreach ($all_f as $k => $flv) {
+                $data_array[$k]['date'] = $flv->created_at;
+                if ($flv->follower_id == $this_user->id) {
+                    $data_array[$k]['type'] = 'follower';
+                } else {
+                    $data_array[$k]['type'] = 'following';
                 }
+                $data_array[$k]['m'] = date('M', strtotime($flv->created_at));
+                $data_array[$k]['j'] = date('j', strtotime($flv->created_at));
+                $user_follower = User::find($flv->follower_id);
+                if ($user_follower) {
+                    $data_array[$k]['email'] = $user_follower->email;
+                    $data_array[$k]['id'] = $user_follower->id;
+                }
+                $count++;
             }
-
-   
-    	}
+        }
 
         return $data_array;
     }
